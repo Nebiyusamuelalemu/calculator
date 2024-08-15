a3a0873dcabb2d58b4c16d27b29435c16f18d3b2
Nebiyusamuelalemu <nebiyusamuelalemu@gmail.com>
2024-08-15 19:45:46 +0300

    k now it's done 🥲

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const display_input = document.getElementById('display');
 const clear_b = document.getElementById('clear');
 let calculated = false;
-let operand_pressed = false;
 const delBtn = document.getElementById('del');
+let notifier = true;
 
+if (notifier){
+    window.alert("This App Was Made By Nebiyu\nSide Note: thank you for using my calculator it was my first project hope you liked it :)")
+    notifier = false;
+}
 
 function del(){
     display_input.value = display_input.value.slice(0, -1);
@@ -74,3 +78,60 @@ function calculate(){
     }
 }
 
+document.addEventListener("keydown", ev => {
+    //console.log(ev.key);
+    if (ev.key == "0"){
+        display('0');
+    }
+    else if (ev.key == "1"){
+        display('1');
+    }
+    else if (ev.key == "2"){
+        display('2');
+    }
+    else if (ev.key == "3"){
+        display('3');
+    }
+    else if (ev.key == "4"){
+        display('4');
+    }
+    else if (ev.key == "5"){
+        display('5');
+    }
+    else if (ev.key == "6"){
+        display('6');
+    }
+    else if (ev.key == "7"){
+        display('7');
+    }
+    else if (ev.key == "8"){
+        display('8');
+    }
+    else if (ev.key == "9"){
+        display('9');
+    }
+    else if (ev.key == "Enter" || ev.key == "="){
+        calculate();
+    }
+    else if (ev.key == "+"){
+        display('+');
+    }
+    else if (ev.key == "-"){
+        display('-');
+    }
+    else if (ev.key == "*"){
+        display('*');
+    }
+    else if (ev.key == "/"){
+        display('/');
+    }
+    else if (ev.key == "c" || ev.key == "C"){
+        clear();
+    }
+    else if (ev.key == "d" || ev.key == "D" || ev.key == "Backspace" || ev.key == "Delete"){
+        del();
+    }
+    else if (ev.key == "."){
+        display('.');
+    }
+})
