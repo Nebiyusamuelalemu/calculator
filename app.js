3a6ff581dd542b0c778e45d5370a78941dddde41
Nebiyusamuelalemu <nebiyusamuelalemu@gmail.com>
2024-08-15 18:19:11 +0300

    Done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-
 const display_input = document.getElementById('display');
 const clear_b = document.getElementById('clear');
+let calculated = false;
 
 
 function clear(){
@@ -10,7 +10,22 @@ function clear(){
 clear_b.onclick = clear
 
 function display(text){
-    display_input.value += text;
+    if(calculated && (text == '+' || text == '-' || text == '*' || text == '/')){
+        display_input.value += text;
+        //console.log("added");
+    }
+    else{
+        if (calculated) {
+            //display_input.value = "";
+            display_input.value = text;
+            //console.log("no value");
+        } 
+        else {
+            display_input.value += text;
+            //console.log("writing ")
+        }
+        calculated = false;
+    }
 }
 
 function calculate(){
@@ -30,6 +45,7 @@ function calculate(){
         else{
             display_input.value = eval(display_input.value);
         }
+        calculated = true;
     }
     catch(error){
         display_input.value = "Error";
